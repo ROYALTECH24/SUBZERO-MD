6db091a229c7282a60dba1e772b1564c09d5a7f0
ROYALTECH24 <roye10969@gmail.com>
2025-08-05 00:00:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;nJc3QbiL#OZiEG-U7P5V2Z-CytZ-FcSWZ0kp7i1-0NPREDVesdCI", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
