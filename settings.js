5ed64467b1f5e576f2997997b029cafc4af6b8f8
ROYALTECH24 <roye10969@gmail.com>
2025-10-12 03:52:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;OJE2WC7J#TpQwVh4Rfy7smFxDSVyNPwwxTNUlFHBSr7dAdtkJ2vM", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
