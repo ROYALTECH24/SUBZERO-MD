7032a10e0d71baa8de4b20ea53fffa47ee534234
ROYALTECH24 <roye10969@gmail.com>
2025-11-10 10:44:41 +0300

    Update settings.js for Mavali

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;OJE2WC7J#TpQwVh4Rfy7smFxDSVyNPwwxTNUlFHBSr7dAdtkJ2vM", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "254799211357", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~gFANyH",
+  OWNER_NUMBER: "+254788085840", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
